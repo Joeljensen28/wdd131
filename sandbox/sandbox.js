c0b65e91c97d9ae855f0e1466e7a65dbbfaf6c31
Joel Jensen <joelsemail28@gmail.com>
2024-07-09 12:00:35 -0600

    Made the site plan

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,3 +1,17 @@
-let esomeprazole = {'id':'mnb78932','amount':23145,'amountType':'ct','expDate':'10/01/2021'}
+function fetchData(callback) {
+    // using setTimeout to simulate fetching data from a server
+    setTimeout(() => {
+      // This calls the 'callback' function and passes data to it.
+      callback('Data has been fetched');
+    }, 2000); // This simulates a 2-second delay from a service.
+  }
+  
+  // function that processes the data
+  function processData(data) {
+    console.log("Data received:", data);
+  }
+  
+  // Call the fetchData function and pass the processData function as an argument.
+  fetchData(processData);
 
-console.log(dose);
+  localStorage.getItem('')
